f6b98402b68665ad0ce9e2716a36a30f10a88f20
Marynaeng <43760699+Marynaeng@users.noreply.github.com>
2018-10-15 22:33:26 +0300

    Update MyFirstProgram.java

--- src/main/java/MyFirstProgram.java
+++ src/main/java/MyFirstProgram.java
@@ -1,5 +1,5 @@
 public class MyFirstProgram {
   public static void main (String [] args){
-     System.out.println ("Hello,world!SDFDSSF"); 
+     System.out.println ("Hello,world! Marynka - developer"); 
 }
 }
