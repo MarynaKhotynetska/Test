9bbdaac9e77e1e9c74ea36030d77db201905342d
Marinka <marynaIvanovna92@gmail.com>
2018-10-22 21:40:42 +0300

    add function vld for .....

---
+++ src/main/java/ua/stqa/test/sandbox/MyFirstProgram.java
@@ -0,0 +1,13 @@
+package ua.stqa.test.sandbox;
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello,world! Marynka - developer");
+    }
+
+
+
+
+
+    public static void vld(){}
+}
